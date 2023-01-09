e1a3f1f26e5a8a3d77f0296e20ca10d3da0c1756
kankanzhou <121850056+kankanzhou@users.noreply.github.com>
2023-01-09 11:34:51 -0800

    Update SystemIn.java

--- Test1/SystemIn.java
+++ Test1/SystemIn.java
@@ -3,13 +3,10 @@ import java.util.Scanner;
 public class SystemIn {
     
     public static void main(String[] args) {  
-       // Scanner scanner = new Scanner(System.in);    
-        //int num = scanner.nextInt(); 
         char c = ' ';
         try {
             c = (char) System.in.read();
         } catch(IOException e) {}
-     //   System.out.println(num);
         System.out.println("this is a char" + c);
     }
 }
