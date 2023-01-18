725331f0220bdce4225ba963b09a52b389dfb807
kankanzhou <zhoukanweekly@outlook.com>
2023-01-18 14:29:57 -0800

    updated

---
+++ Lecture3/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: KanZhou
+ * @Date: ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        Person man = new Person("kan", 20);
+        man.sayHello();
+        System.out.println("Hello world!");
+    }
+}
