35b3c6ac54293d8247be735d0753a3effe729e90
kankanzhou <zhoukanweekly@outlook.com>
2023-02-13 22:32:11 -0800

    updated

--- Demo1/src/Demo2/Demo2Class.java
+++ Demo1/src/Demo2/Demo2Class.java
@@ -5,10 +5,31 @@ package Demo2;
  * @Date: 2023-02-13 7:45 p.m.
  */
 public class Demo2Class {
+    public static void printArray(int[] array) {
+        System.out.println("=================");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println("=================");
+    }
+
+    public static int[] cal(int a, int b, int c) {
+        int sum = a + b + c;
+        int avg = sum / 3;
+        int[] arr = new int[2];
+        arr[0] = sum;
+        arr[1] = avg;
+        return arr;
+    }
+
     public static void main(String[] args) {
-        int[] array = {15, 25, 35};
-        System.out.println(array[0]);
-        System.out.println(array[1]);
-        System.out.println(array[2]);
+        int[] array = {5, 15, 1, 20, 10};
+        for (int i = 0, j = array.length - 1; i < j; i++, j--) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        int[] arr = cal(1, 2, 3);
+        printArray(arr);
     }
 }
