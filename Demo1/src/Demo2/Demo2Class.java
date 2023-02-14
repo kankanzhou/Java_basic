c622d9182312c79fecf2d515e7efa4f7dc449e8b
kankanzhou <zhoukanweekly@outlook.com>
2023-02-13 20:17:07 -0800

    updated

---
+++ Demo1/src/Demo2/Demo2Class.java
@@ -0,0 +1,14 @@
+package Demo2;
+
+/**
+ * @Author: KanZhou
+ * @Date: 2023-02-13 7:45 p.m.
+ */
+public class Demo2Class {
+    public static void main(String[] args) {
+        int[] array = {15, 25, 35};
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+    }
+}
