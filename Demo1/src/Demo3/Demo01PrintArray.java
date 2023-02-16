6bc5013fd9c1154bfa79f12cf302321d2f9f3292
kankanzhou <zhoukanweekly@outlook.com>
2023-02-16 02:56:01 -0800

    updated

---
+++ Demo1/src/Demo3/Demo01PrintArray.java
@@ -0,0 +1,15 @@
+package Demo3;
+
+import java.util.Arrays;
+
+/**
+ * @Author: KanZhou
+ * @Date: 2023-02-16 2:48 a.m.
+ */
+public class Demo01PrintArray {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(array));
+    }
+}
+
