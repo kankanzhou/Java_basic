a86f09a2eea5e51c3ae5e5681259b298ab2435b4
kankanzhou <zhoukanweekly@outlook.com>
2023-02-17 11:33:10 -0800

    updated

---
+++ Demo1/src/Demo6/Ios.java
@@ -0,0 +1,15 @@
+package Demo6;
+
+import java.util.Scanner;
+
+/**
+ * @Author: KanZhou
+ * @Date: 2023-02-17 11:21 a.m.
+ */
+public class Ios {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //int num = sc.nextInt();
+        System.out.println(sc.next());
+    }
+}
