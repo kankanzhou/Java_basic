fd98eba73dadab2064bbb1a1914c725b9a95be38
kankanzhou <zhoukanweekly@outlook.com>
2023-02-18 13:58:19 -0800

    updated

---
+++ Demo3/src/Demo1/Test1.java
@@ -0,0 +1,24 @@
+package Demo1;
+
+/**
+ * @Author: KanZhou
+ * @Date: 2023-02-18 1:43 p.m.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = "abc";
+        char[] s3 = { 'a', 'b', 'c' };
+        String s4 = new String(s3);
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s4);
+        System.out.println(s2 == s4);
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s4));
+        System.out.println(s4.equals(s2));
+        System.out.println(s3.equals(s2));
+        System.out.println(s1.equals("abc"));
+        System.out.println("abc".equals(s2));
+
+    }
+}
